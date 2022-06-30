6e24cf3bc351ae2a59383278d2a4d508f59c254c
Gustavo Henrique Baumann <108095242+Gustavo-Baumann@users.noreply.github.com>
2022-06-30 17:54:16 -0300

    +boxes
    Adicionei uns quadrado

---
+++ teste_html/script.js
@@ -0,0 +1,23 @@
+function spin(){
+    document.getElementById("box1").innerHTML = "<p>Around</p>"
+}
+
+function stop1(){
+    document.getElementById("box1").innerHTML = "<p>Box</p>"
+}
+
+function big(){
+    document.getElementById("box3").innerHTML = "<p style='top: 10px'>Big<br>Box</p>"
+}
+
+function stop2(){
+    document.getElementById("box3").innerHTML = "<p>Box</p>"
+}
+
+function rainbow(){
+    document.getElementById("box4").innerHTML = "<p style='top: 10px'>Rainbow Box</p>"
+}
+
+function stop3(){
+    document.getElementById("box4").innerHTML = "<p>Box</p>"
+}
