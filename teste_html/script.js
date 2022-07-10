239b0b86b98fb111068514239099577e73ec7d73
Gustavo Henrique Baumann <108095242+Gustavo-Baumann@users.noreply.github.com>
2022-07-10 12:56:34 -0300

    juntando funções
    --

--- teste_html/script.js
+++ teste_html/script.js
@@ -2,31 +2,25 @@ function spin(){
     document.getElementById("box1").innerHTML = "<p>Around</p>"
 }
 
-function stop1(){
+function stop(){
     document.getElementById("box1").innerHTML = "<p>Box</p>"
+    document.getElementById("box3").innerHTML = "<p>Box</p>"
+    document.getElementById("box4").innerHTML = "<p>Box</p>"
 }
 
 function big(){
     document.getElementById("box3").innerHTML = "<p style='top: 10px'>Big<br>Box</p>"
 }
 
-function stop2(){
-    document.getElementById("box3").innerHTML = "<p>Box</p>"
-}
-
 function rainbow(){
     document.getElementById("box4").innerHTML = "<p style='top: 10px'>Rainbow Box</p>"
 }
 
-function stop3(){
-    document.getElementById("box4").innerHTML = "<p>Box</p>"
-}
-
 function mudacor(){
-    if(window.document.getElementsByTagName('body')[0].style.backgroundColor == 'grey'){
-        window.document.getElementsByTagName('body')[0].style.backgroundColor = 'orange'
-    }else{
+    if(window.document.getElementsByTagName('body')[0].style.backgroundColor == 'orange'){
         window.document.getElementsByTagName('body')[0].style.backgroundColor = 'grey'
+    }else{
+        window.document.getElementsByTagName('body')[0].style.backgroundColor = 'orange'
     }
 }
 
